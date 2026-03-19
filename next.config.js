072ee597d2c0067674dc99d79513f7af3e39b58d
sibi-narendran <sibinarendran@gmail.com>
2026-03-19 19:09:54 +0530

    ok push this to production

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'cdn.outrank.so',
       },
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+      },
     ],
   },
 
